b509d37a7b7b7ef53f7cca9c1a5152f5454225ea
Aman <dalmi.aman@gmail.com>
2025-01-15 13:39:11 +0530

    feat(iam): add JWT configuration and integrate JwtModule into IamModule

---
+++ src/iam/config/jwt.config.js
@@ -0,0 +1,20 @@
+// jwt.config.ts
+import { registerAs, ConfigService } from '@nestjs/config';
+
+export default registerAs('jwt', () => {
+  const configService = new ConfigService();
+  const secret = configService.get('JWT_SECRET');
+  if (!secret) {
+    throw new Error('JWT_SECRET environment variable is not set');
+  }
+
+  return {
+    secret,
+    audience: configService.get('JWT_TOKEN_AUDIENCE'),
+    issuer: configService.get('JWT_TOKEN_ISSUER'),
+    accessTokenTtl: parseInt(
+      configService.get('JWT_ACCESS_TOKEN_TTL') ?? '3600',
+      10,
+    ),
+  };
+});
--- src/iam/iam.module.ts
+++ src/iam/iam.module.ts
@@ -5,9 +5,16 @@ import { AuthController } from './auth/auth.controller';
 import { AuthService } from './auth/auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/users/entities/user.entity';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule } from '@nestjs/config';
+import jwtConfig from './config/jwt.config';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    JwtModule.registerAsync(jwtConfig.asProvider()),
+    ConfigModule.forFeature(jwtConfig),
+  ],
   providers: [
     {
       provide: HashingService,
