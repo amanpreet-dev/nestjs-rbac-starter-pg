d1f9d530f0f033baff482d4e37522086ffb8a4f3
Aman <dalmi.aman@gmail.com>
2025-01-15 11:56:47 +0530

    feat(auth): add AuthController and AuthService with basic structure and DTOs

--- src/iam/iam.module.ts
+++ src/iam/iam.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { HashingService } from './hashing/hashing.service';
 import { BcryptService } from './hashing/bcrypt.service';
+import { AuthController } from './auth/auth.controller';
+import { AuthService } from './auth/auth.service';
 
 @Module({
   providers: [
@@ -8,6 +10,8 @@ import { BcryptService } from './hashing/bcrypt.service';
       provide: HashingService,
       useClass: BcryptService,
     },
+    AuthService,
   ],
+  controllers: [AuthController],
 })
 export class IamModule {}
